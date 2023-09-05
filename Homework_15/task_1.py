ec1921a176f90437bc6e470556eb4f5e112c9634
Jusinka <shaurma123123123@gmail.com>
2023-09-05 15:56:58 +0300

    homework

--- Homework_15/task_1.py
+++ Homework_15/task_1.py
@@ -18,17 +18,17 @@ class MultipleOptionsError(Exception):
 
 
 def search_airport(data_file, **kwargs):
-    # Валідація аргументів
+
     if not kwargs:
         raise NoOptionsFoundError("No search parameter provided.")
     if len(kwargs) > 1:
         raise MultipleOptionsError("Only one search parameter is allowed.")
 
-    # Відкриття CSV-файлу з кодуванням UTF-8
+
     with open(data_file, 'r', encoding='utf-8') as csvfile:
         reader = csv.DictReader(csvfile)
 
-        # Пошук за кодом IATA
+
         iata_code = kwargs.get('iata_code')
         if iata_code:
             if not isinstance(iata_code, str) or len(iata_code) != 3 or not iata_code.isupper():
@@ -40,7 +40,7 @@ def search_airport(data_file, **kwargs):
 
             raise AirportNotFoundError("Airport not found", iata_code)
 
-        # Пошук за країною
+
         country = kwargs.get('country')
         if country:
             results = [row for row in reader if row['iso_country'] == country]
@@ -48,7 +48,7 @@ def search_airport(data_file, **kwargs):
                 raise CountryNotFoundError("Country not found", country)
             return results
 
-        # Пошук за іменем
+
         name = kwargs.get('name')
         if name:
             results = [row for row in reader if name.lower() in row['name'].lower()]
@@ -58,7 +58,25 @@ def search_airport(data_file, **kwargs):
 
 
 try:
-    result = search_airport('airport-codes_csv.csv', iata_code='NIS')
+    result = search_airport('airport-codes_csv.csv', iata_code='NS')
     print(result)
 except (AirportNotFoundError, CountryNotFoundError, NoOptionsFoundError, MultipleOptionsError) as e:
+    print(e)
+
+try:
+    result = search_airport('airport-codes_csv.csv', iata_code='JFK', country='US')
+    print(result)
+except MultipleOptionsError as e:
+    print(e)
+
+try:
+    result = search_airport('airport-codes_csv.csv')
+    print(result)
+except NoOptionsFoundError as e:
+    print(e)
+
+try:
+    result = search_airport('airport-codes_csv.csv', country='XYZ')
+    print(result)
+except CountryNotFoundError as e:
     print(e)
