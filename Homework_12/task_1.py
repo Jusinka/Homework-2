30397e20729e496729c24058053a055a29c67159
Jusinka <shaurma123123123@gmail.com>
2023-09-11 22:56:11 +0300

    homework

--- Homework_12/task_1.py
+++ Homework_12/task_1.py
@@ -32,14 +32,50 @@ class City:
             for building in street.buildings:
                 print(f"   {street.number}        {building.number}         {building.population}")
 
-# Створюємо місто
+    def add_street(self):
+        new_street = Street(len(self.streets) + 1)
+        self.streets.append(new_street)
+
+    def remove_street(self, street_number):
+        if 1 <= street_number <= len(self.streets):
+            del self.streets[street_number - 1]
+        else:
+            print("Недопустимый номер улицы")
+
+    def add_building(self, street_number):
+        if 1 <= street_number <= len(self.streets):
+            street = self.streets[street_number - 1]
+            new_building = Building(len(street.buildings) + 1)
+            street.buildings.append(new_building)
+        else:
+            print("Недопустимый номер улицы")
+
+    def remove_building(self, street_number, building_number):
+        if 1 <= street_number <= len(self.streets):
+            street = self.streets[street_number - 1]
+            if 1 <= building_number <= len(street.buildings):
+                del street.buildings[building_number - 1]
+            else:
+                print("Недопустимый номер здания")
+        else:
+            print("Недопустимый номер улицы")
+
+# Создаем город
 city = City()
 
-# Заповнюємо місто будинками та населенням
+# Заполняем город зданиями и населением
 city.populate_city()
 
-# Виводимо загальне населення міста
+# Выводим общее население города
 print(f"Загальне населення міста: {city.total_population()}")
 
-# Друкуємо таблицю міста
+# Добавляем новую улицу и здание
+city.add_street()
+city.add_building(4)  # Добавляем здание на четвертой улице
+
+# Удаляем улицу и здание
+city.remove_street(2)  # Удаляем вторую улицу
+city.remove_building(2, 2)  # Удаляем второе здание на третьей улице
+
+# Печатаем таблицу города после изменений
 city.print_city_table()
